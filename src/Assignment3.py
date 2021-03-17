733534c109786ebea74e898f3b829f6f46f13a4a
Pandaxubo <jiangxubo@gmail.com>
2021-03-17 11:55:56 -0400

    Finish 3/4 of Assignment 3

--- src/Assignment3.py
+++ src/Assignment3.py
@@ -22,25 +22,34 @@ class Assignment3:
         plt.imshow(img, cmap = 'gray')
         plt.show()
 
-        self.B = img
+        return img
 
     ### threshold image
-    def _threshold_image(self, threshold):
-        target = np.copy(self.B)
-        for x in range(self.w):
-            for y in range(self.h):
+    def _threshold_image(self, threshold, img):
+        target = np.copy(img)
+        for x in range(target.shape[0]):
+            for y in range(target.shape[1]):
                 if target[x][y] >= threshold:
                     target[x][y] = 255
                 else:
                     target[x][y] = 0
+
         return target
+
+    ### display image (If Threshold is False, that means image is thresholded.)
+    def _display_image(self, String, T, img, threshold : bool):
+        if threshold == True:
+            img = self._threshold_image(T, img)
+        print(String)
+        plt.imshow(img, cmap='gray')
+        plt.show()
 
     ########################################################################
     ### 1. Thresholding using peakiness detection
-    def _peakiness_detection(self):
+    def _peakiness_detection(self, img):
 
         ### generate histogram
-        histogram = np.round(np.transpose(cv.calcHist([self.B], [0], None, [256], [0,256]))).astype(int)[0].tolist()
+        histogram = np.round(np.transpose(cv.calcHist([img], [0], None, [256], [0,256]))).astype(int)[0].tolist()
 
         ### store final peakiness
         peakiness_threshold = 0
@@ -73,13 +82,11 @@ class Assignment3:
         T = max(peakiness)
 
         ### display image
-        peakinessed_image = self._threshold_image(T)
-        print("After peakiness:")
-        plt.imshow(peakinessed_image, cmap='gray')
-        plt.show()
+        self._display_image("Peakiness Detection", T, img, True)
 
     ### select neighbor for each peak for constructing gi_gj(For peakiness thresholding)
     def _select_neighbor(self, peaks, i):
+        
         return peaks[i - 1] if abs(peaks[i - 1] - peaks[i]) > abs(peaks[i + 1] - peaks[i]) else peaks[i + 1]
 
     ### find lowest point between gi and gj(For peakiness thresholding)
@@ -87,17 +94,18 @@ class Assignment3:
         point = histogram[min(gi, gj)]
         for k in range(min(gi, gj), max(gi, gj)):
             point = min(histogram[k], point)
+
         return point
 
     ########################################################################
-    ### 2. Iterative thresholding
-    def _iterative_thresholding(self):
+    ### 2. Iterative thresholding(Display is explained in the code)
+    def _iterative_thresholding(self, img, display: bool):
 
         ### initial estimate of T is the average of intensity values in B 
-        estimate_T = math.ceil(np.mean(self.B))
+        estimate_T = math.ceil(np.mean(img))
         
         ### initialize a copy of B  
-        temp_B = np.copy(self.B)
+        temp_B = np.copy(img)
 
         ### calculate threshold. iter_list[0, 1, 2] respond to u1, u2 and new_T for each iteration
         iter_list = self._iter_T(temp_B, estimate_T)
@@ -109,13 +117,15 @@ class Assignment3:
             iter_list = self._iter_T(temp_B, iter_list[2])
         
         ### get T in final iteration
-        T = round(iter_list[2])
+        T = math.ceil(iter_list[2])
 
         ### display image
-        thresholded_image = self._threshold_image(T)
-        print("After iterative thresholding:")
-        plt.imshow(thresholded_image, cmap='gray')
-        plt.show()
+        if display == True:
+            self._display_image("Iterative Thresholding", T, img, True)
+
+        ### This part is for Adaptive Thresholding. Display function called in that part. 
+        if display == False:
+            return self._threshold_image(T, img)
         
     ### do iterations to find final T (For iterative thresholding)
     def _iter_T(self, temp_B, current_T):
@@ -123,8 +133,8 @@ class Assignment3:
         ### initialize R1, R2 to store split area
         R1 = []
         R2 = []
-        for x in range(self.w):
-            for y in range(self.h):
+        for x in range(temp_B.shape[0]):
+            for y in range(temp_B.shape[1]):
                 if temp_B[x][y] < current_T:
                     R1.append(temp_B[x][y])
                 else:
@@ -137,9 +147,85 @@ class Assignment3:
 
         return [u1, u2, new_T]
 
+    ########################################################################
+    ### 3. Adaptive thresholding(split original image to 4 equal size sub-images)
+    def _adaptive_thresholding(self, img):
+        
+        ### split 4 sub-images and merge to target image
+        sub1, sub2, sub3, sub4 = self._split_image(img)
+        sub1 = self._iterative_thresholding(sub1, False)
+        sub2 = self._iterative_thresholding(sub2, False)
+        sub3 = self._iterative_thresholding(sub3, False)
+        sub4 = self._iterative_thresholding(sub4, False)
+        target_image = self._merge_image(sub1, sub2, sub3, sub4)
+
+        ### display image. Thresholding value is useless since it has been thresholded when merging it.
+        self._display_image("Adaptive thresholding", 0, target_image, False)
+
+
+    ### split original image to 4 equal size sub-images(For adaptive thresholding)
+    def _split_image(self, img):
+        
+        ### if we don`t use int here will raise error
+        self.half_w = int(self.w / 2)
+        self.half_h = int(self.h / 2)
+        self.full_w = int(self.w)
+        self.full_h = int(self.h)
+
+        ### initialize 4 sub-images
+        sub1 = np.zeros((self.half_w, self.half_h), dtype='uint8')
+        sub2 = np.zeros((self.half_w, self.half_h), dtype='uint8')
+        sub3 = np.zeros((self.half_w, self.half_h), dtype='uint8')
+        sub4 = np.zeros((self.half_w, self.half_h), dtype='uint8')
+
+        ### value 4 sub-images
+        for x in range(self.half_w):
+            for y in range(self.half_h):
+                sub1[x][y] = img[x][y]
+
+        for x in range(self.half_w, self.full_w):
+            for y in range(0, self.half_h):
+                sub2[x - self.half_w][y] = img[x][y]
+
+        for x in range(self.half_w):
+            for y in range(self.half_h, self.full_h):
+                sub3[x][y - self.half_h] = img[x][y]
+
+        for x in range(self.half_w, self.full_w):
+            for y in range(self.half_h, self.full_h):
+                sub4[x - self.half_w][y - self.half_h] = img[x][y]
+
+        return sub1, sub2, sub3, sub4
+
+    ### merge 4 sub-images into target image(For adaptive thresholding)
+    def _merge_image(self, sub1, sub2, sub3, sub4):
+
+        ### initialize target threshold image
+        target_image = np.zeros((self.full_w, self.full_h), dtype='uint8')
+
+        ### value target image
+        for x in range(self.half_w):
+            for y in range(self.half_h):
+                target_image[x][y] = sub1[x][y]
+
+        for x in range(self.half_w, self.full_w):
+            for y in range(self.half_h):
+                target_image[x][y] = sub2[x - self.half_w][y]
+
+        for x in range(self.half_w):
+            for y in range(self.half_h, self.full_h):
+                target_image[x][y] = sub3[x][y - self.half_h]
+
+        for x in range(self.half_w, self.full_w):
+            for y in range(self.half_h, self.full_h):
+                target_image[x][y] = sub4[x - self.half_w][y - self.half_h]
+        
+        return target_image
+
 
 result = Assignment3("Data/test1.img")
-result._get_original_image()
-result._peakiness_detection()
-result._iterative_thresholding()
+img = result._get_original_image()
+result._peakiness_detection(img)
+result._iterative_thresholding(img, True)
+result._adaptive_thresholding(img)
 
